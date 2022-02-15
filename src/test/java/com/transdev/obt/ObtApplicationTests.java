18b938e8a67b6cb6ebc34c8379e597fb583e504b
Amokrane ZATOUT <amokrane.zatout@gmail.com>
2022-02-15 18:37:30 +0100

    Initial commit will set the project structure

---
+++ src/test/java/com/transdev/obt/ObtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transdev.obt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
